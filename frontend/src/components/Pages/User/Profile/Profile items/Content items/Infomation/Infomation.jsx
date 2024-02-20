c620566873c5cef08d2dc78ae56d244e8e4fcebb
Pi2608 <ibaxq26@gmail.com>
2024-02-20 12:48:44 +0700

    userpage

--- frontend/src/components/Pages/User/Profile/Profile items/Content items/Infomation/Infomation.jsx
+++ frontend/src/components/Pages/User/Profile/Profile items/Content items/Infomation/Infomation.jsx
@@ -1,7 +1,9 @@
 import React,{ useState } from "react";
+import TextField from '@mui/material/TextField';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import StarsIcon from '@mui/icons-material/Stars';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 import "./Infomation.css"
 
 export default function Infomation() {
@@ -9,6 +11,8 @@ export default function Infomation() {
     const [userName, setUserName] = useState('Pi26');
     const [userPoint, setUserPoint] = useState(0);
     const [userCredit, setUserCredit] = useState('0.0 đ');
+    const [userPhoneNumber, setUserPhoneNumber] = useState('0867545781');
+    const [userEmail, setUserEmail] = useState('datldpse173640@fpt.edu.vn');
 
     return (
         <div id="info">
@@ -38,23 +42,72 @@ export default function Infomation() {
                         <div className="info-box">
                             <div className="info-box-item">
                                 <p>Ngày sinh</p>
-                                <p>--/--/----</p>
+                                <p className="main">--/--/----</p>
                             </div>
                             <div className="info-box-item">
                                 <p>Giới tính</p>
-                                <p>Nam</p>
+                                <p className="main">Nam</p>
                             </div>
                         </div>
                         <div className="info-desc">
                             <div className="info-desc-item">
                                 <p>Số điện thoại</p>
-                                <p></p>
+                                <p className="main">{userPhoneNumber}</p>
                             </div>
                             <div className="info-desc-item">
                                 <p>Email</p>
-                                <p></p>
+                                <p className="main">{userEmail}</p>
                             </div> 
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="title">
+                    <div>
+                        <h2 style={{paddingRight: "8px"}}>Giấy phép lái xe</h2>
+                        <div style={{fontSize: "10px", padding:'8px'}}>
+                            <div style={{backgroundColor: "#FF6A6A", borderRadius:"8px", padding: "3px"}}>
+                                <HighlightOffOutlinedIcon style={{height: "10px", width: "12px", color:"red"}}/>
+                                <p>Chưa xác thực</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>Chỉnh sửa</div>
+                </div>
+                <div className="content">
+                    <div className="info-license">
+                        <h3>Thông tin chung</h3>
+                        <h4 style={{color: "#767676"}}>Số GPLX</h4>                        
+                        <TextField 
+                            placeholder="Số GPLX"
+                            size="small"
+                            sx={{
+                                width: "400px",
+                            }}
+                            />
+                        <h4 style={{color: "#767676"}}>Họ và tên</h4>
+                        <TextField 
+                            placeholder="Họ và tên"
+                            size="small"
+                            sx={{
+                                width: "400px",
+                            }}
+                            />
+                        <h4 style={{color: "#767676"}}>Ngày sinh</h4>
+                        <TextField 
+                            placeholder="--/--/----"
+                            size="small"
+                            sx={{
+                                width: "400px",
+                            }}
+                            />
+                    </div>
+                    <div className="info-license">
+                        <h3>Hình ảnh</h3>
+                        <label className="info-license-upload">
+
+                        </label>
                     </div>
                 </div>
             </div>
