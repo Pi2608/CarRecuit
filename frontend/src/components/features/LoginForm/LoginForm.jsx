b5334048f604f503ea90cd4b39d01bea51754da5
Vinh0209 <vanvinhhh2903@gmail.com>
2024-02-21 12:31:03 +0700

    upload LoginForm and Header

---
+++ frontend/src/components/features/LoginForm/LoginForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {FaUser, FaLock } from 'react-icons/fa';
+import "./LoginForm.css";
+import Login from "../Login/Login";
+
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Đăng nhập</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Email/Số điện thoại" required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Mật khẩu" required />
+          <FaLock className="icon" />
+        </div>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" />
+            Ghi nhớ
+          </label>
+          <a href="#">Quên mật khẩu?</a>
+        </div>
+        <button type="submit">Đăng nhập</button>
+        <div className="register-link">
+          <p>
+            Bạn chưa là thành viên? <a href="#">Đăng ký</a>
+          </p>
+        </div>
+        <Login/>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm
