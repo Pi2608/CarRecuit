15633bc7ab70cc0dbefccff85e7106eacd614b2f
Pi2608 <ibaxq26@gmail.com>
2024-02-26 17:09:18 +0700

    fix the User interface

--- frontend/src/components/Pages/User/Profile/Profile items/Content items/Infomation/Infomation.jsx
+++ frontend/src/components/Pages/User/Profile/Profile items/Content items/Infomation/Infomation.jsx
@@ -10,7 +10,9 @@ import "./Infomation.css"
 export default function Infomation() {
 
     const fileInputRef = useRef(null);
-    const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile1, setSelectedFile1] = useState(null);
+    const [selectedFile2, setSelectedFile2] = useState(null);
+    const [selectedFile3, setSelectedFile3] = useState(null);
     const [fileUploadSuccess, setFileUploadSuccess] = useState(false);
     const [userName, setUserName] = useState('Pi26');
     const [userPoint, setUserPoint] = useState(0);
@@ -27,27 +29,46 @@ export default function Infomation() {
         fileInputRef.current.click();
     };
     
-    const handleFileChange = (event) => {
+    const handleFileChange1 = (event) => {
         try {
           const file = event.target.files[0];
           if (!file) {
             throw new Error("No file selected");
           }
-    
-          // Your logic for handling the selected file (e.g., uploading to a server, processing, etc.)
           console.log("File selected:", file.name);
-    
-          // Set the selected file to display
-          setSelectedFile(file);
-          
-          // Set any success indicator in your state or trigger success-related logic
+          setSelectedFile1(file);
           setFileUploadSuccess(true);
         } catch (error) {
-          // Handle any errors or failures
           console.error("Error:", error.message);
-    
-          // Set any error indicator in your state or trigger error-related logic
           setFileUploadSuccess(false);
+        }
+    };
+
+    const handleFileChange2 = (event) => {
+        try {
+          const file = event.target.files[0];
+          if (!file) {
+            throw new Error("No file selected");
+          }
+          console.log("File selected:", file.name);
+          setSelectedFile2(file);
+          setFileUploadSuccess(true);
+        } catch (error) {
+          console.error("Error:", error.message);
+          setFileUploadSuccess(false);
+        }
+    };
+
+    const handleFileChange3 = (event) => {
+        try {
+          const file = event.target.files[0];
+          if (!file) {
+            throw new Error("No file selected");
+          }
+          console.log("File selected:", file.name);
+          setSelectedFile3(file);
+        } catch (error) {
+          console.error("Error:", error.message);
         }
     };
 
@@ -178,9 +199,102 @@ export default function Infomation() {
                     </div>
                     <div className="info-id-card">
                         <h3>Hình ảnh</h3>
-                            <label className="info-license-upload">
-
+                        <p>Mặt trước:</p>
+                        {selectedFile1 ? (
+                            <div className="img-container">
+                                <label htmlFor="input-file1" className="info-license-upload" style={{maxWidth:"100%", maxHeight:"240px", overflow: "hidden"}}>
+                                    <img src={URL.createObjectURL(selectedFile1)} style={{width: "100%", height: "auto"}} className="uploaded-image"/>
+                                    <button
+                                        id="upload-ndl"
+                                        type="button"
+                                        onClick={() => {
+                                        handleClick();
+                                        }}
+                                        style={{ display: "none" }}
+                                    >
+                                        Thêm ảnh
+                                    </button>
+                                    <input
+                                        id="input-file1"
+                                        type="file"
+                                        style={{ display: "none" }}
+                                        ref={fileInputRef}
+                                        accept=".png, .jpg"
+                                        onChange={handleFileChange1} 
+                                    />
+                                </label>
+                            </div>
+                        ) : (
+                            <label htmlFor="input-file1" className="info-license-upload">
+                                <CameraAltOutlinedIcon fontSize="large" />
+                                <button
+                                    id="upload-ndl"
+                                    type="button"
+                                    onClick={() => {
+                                    handleClick();
+                                    }}
+                                    style={{ display: "none" }}
+                                >
+                                    Thêm ảnh
+                                </button>
+                                <input
+                                    id="input-file1"
+                                    type="file"
+                                    style={{ display: "none" }}
+                                    ref={fileInputRef}
+                                    accept=".png, .jpg"
+                                    onChange={handleFileChange1} 
+                                />
                             </label>
+                        )}
+                        <p>Mặt sau:</p>
+                        {selectedFile2 ? (
+                            <div className="img-container">
+                                <label htmlFor="input-file2" className="info-license-upload" style={{maxWidth:"100%", maxHeight:"240px", overflow: "hidden"}}>
+                                    <img src={URL.createObjectURL(selectedFile2)} style={{width: "100%", height: "auto"}} className="uploaded-image"/>
+                                    <button
+                                        id="upload-ndl"
+                                        type="button"
+                                        onClick={() => {
+                                        handleClick();
+                                        }}
+                                        style={{ display: "none" }}
+                                    >
+                                        Thêm ảnh
+                                    </button>
+                                    <input
+                                        id="input-file2"
+                                        type="file"
+                                        style={{ display: "none" }}
+                                        ref={fileInputRef}
+                                        accept=".png, .jpg"
+                                        onChange={handleFileChange2} 
+                                    />
+                                </label>
+                            </div>
+                        ) : (
+                            <label htmlFor="input-file2" className="info-license-upload">
+                                <CameraAltOutlinedIcon fontSize="large" />
+                                <button
+                                    id="upload-ndl"
+                                    type="button"
+                                    onClick={() => {
+                                    handleClick();
+                                    }}
+                                    style={{ display: "none" }}
+                                >
+                                    Thêm ảnh
+                                </button>
+                                <input
+                                    id="input-file2"
+                                    type="file"
+                                    style={{ display: "none" }}
+                                    ref={fileInputRef}
+                                    accept=".png, .jpg"
+                                    onChange={handleFileChange2} 
+                                />
+                            </label>
+                        )}
                     </div>
                 </div>
             </div>
@@ -228,10 +342,10 @@ export default function Infomation() {
                     </div>
                     <div className="info-license">
                     <h3>Hình ảnh</h3>
-                        {selectedFile ? (
+                        {selectedFile3 ? (
                             <div className="img-container">
-                                <label htmlFor="input-file" className="info-license-upload" style={{maxWidth:"100%", maxHeight:"240px", overflow: "hidden"}}>
-                                    <img src={URL.createObjectURL(selectedFile)} style={{width: "100%", height: "auto"}} className="uploaded-image"/>
+                                <label htmlFor="input-file3" className="info-license-upload" style={{maxWidth:"100%", maxHeight:"240px", overflow: "hidden"}}>
+                                    <img src={URL.createObjectURL(selectedFile3)} style={{width: "100%", height: "auto"}} className="uploaded-image"/>
                                     <button
                                         id="upload-ndl"
                                         type="button"
@@ -243,17 +357,17 @@ export default function Infomation() {
                                         Thêm ảnh
                                     </button>
                                     <input
-                                        id="input-file"
+                                        id="input-file3"
                                         type="file"
                                         style={{ display: "none" }}
                                         ref={fileInputRef}
                                         accept=".png, .jpg"
-                                        onChange={handleFileChange} 
+                                        onChange={handleFileChange3} 
                                     />
                                 </label>
                             </div>
                         ) : (
-                            <label htmlFor="input-file" className="info-license-upload">
+                            <label htmlFor="input-file3" className="info-license-upload">
                                 <CameraAltOutlinedIcon fontSize="large" />
                                 <button
                                     id="upload-ndl"
@@ -266,12 +380,12 @@ export default function Infomation() {
                                     Thêm ảnh
                                 </button>
                                 <input
-                                    id="input-file"
+                                    id="input-file3"
                                     type="file"
                                     style={{ display: "none" }}
                                     ref={fileInputRef}
                                     accept=".png, .jpg"
-                                    onChange={handleFileChange} 
+                                    onChange={handleFileChange3} 
                                 />
                             </label>
                         )}
